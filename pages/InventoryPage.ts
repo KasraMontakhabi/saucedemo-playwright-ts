cea7ab457b0867d7e769fb9b69262316343ef34d
kasra <montakhabikasra@gmail.com>
2025-09-10 21:07:12 +0300

    Add InventoryPage class to manage product interactions and retrieve prices

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,31 @@
+import { Locator, Page } from "playwright";
+
+class InventoryPage {
+
+    products: Locator;
+    addToCartButton: Locator;
+    itemPrice: Locator;
+    itemName: Locator;
+
+    constructor(public page: Page) {
+        this.products = page.getByTestId('inventory-item');
+        this.addToCartButton = page.getByRole('button', { name: 'Add to cart' });
+        this.itemName = page.getByTestId('inventory-item-name');
+        this.itemPrice = page.getByTestId('inventory-item-price');
+    }
+
+    async addProductToCart(productName: string) {
+        this.products.filter({ hasText: productName })
+        .first()
+        .filter({ has: this.addToCartButton })
+        .click();
+    }
+
+    async getProductPrice(productName: string){
+        return this.products.filter({ hasText: productName })
+        .first()
+        .filter({ has: this.itemPrice })
+        .innerText();
+    }
+}
+export default InventoryPage;
