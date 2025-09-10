c5f85d14065fb7614792c407dcb52a06e90adb84
kasra <montakhabikasra@gmail.com>
2025-09-10 21:44:38 +0300

    Add Header class to manage page header elements and interactions

---
+++ pages/Header.ts
@@ -0,0 +1,40 @@
+import { Locator, Page } from "@playwright/test";
+
+class Header {
+
+    shoppingCartButton: Locator;
+    menuButton: Locator;
+    pageTitle: Locator;
+    productSortDropdown: Locator;
+    backToProductsButton: Locator;
+
+    constructor(public page: Page) {
+        this.shoppingCartButton = page.getByTestId('shopping-cart-link');
+        this.menuButton = page.getByRole('button', { name: 'Open Menu' });
+        this.pageTitle = page.getByText('Swag Labs');
+        this.productSortDropdown = page.getByRole('combobox');
+        this.backToProductsButton = page.getByRole('button', { name: 'Back to products' });
+    }
+
+    async goToCart() {
+        await this.shoppingCartButton.click();
+    }
+
+    async openMenu() {
+        await this.menuButton.click();
+    }
+
+    async getPageTitle() {
+        return await this.pageTitle.innerText();
+    }
+
+    async sortProducts(sortOption: string) {
+        await this.productSortDropdown.selectOption(sortOption);
+    }
+    
+    async backToProducts() {
+        await this.backToProductsButton.click();
+    }
+
+}
+export default Header;
